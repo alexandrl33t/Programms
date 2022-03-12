1907344d1fe24f6758e9103b148ae8606fb1dceb
Алесандр <99385938+alexandrl33t@users.noreply.github.com>
2022-03-12 22:25:37 +0300

    update2

--- from_french_with_love.py
+++ from_french_with_love.py
@@ -18,7 +18,7 @@ class Main(tk.Frame):
         self.entry.pack(side=TOP)
         self.btn = tk.Button(root, text='Перевод', font=('Times New Roman', 14, BOLD), command=self.to_french)
         self.btn.pack(side=TOP, pady=20) 
-        self.lbl_result = tk.Label(root, text='', font=('Times New Roman', 20, BOLD)) 
+        self.lbl_result = tk.Label(root, text='', font=('Times New Roman', 20, BOLD), wraplength=700) 
         self.lbl_result.pack(side=TOP)
 
 
@@ -26,5 +26,5 @@ if __name__=='__main__':
     root = tk.Tk()
     app = Main(root)
     root.title('Перевод чисел прописью в римские')
-    root.geometry('550x250+300+250')
+    root.geometry('750x250+300+250')
     root.mainloop()

05d9a7380c7eb3aa917313442fa50318378144c4
Alexandr <alexandrl33t@yandex.ru>
2022-05-03 20:34:03 +0300

    new

--- parsing/parsing.py
+++ parsing/parsing.py
@@ -10,34 +10,27 @@ def toInt(command):
         command = int(command)
         return str(command)
     except: 
-        output = "zОшибка. Метка должна быть целым числом. f"
+        return "zОшибка. Метка должна быть целым числом. f"
 
 def command_processing(commands, number_of_command):
     if commands[0] != "ввод":
         output =  "zОшибка. Звено должно содержать слово ввод. f" + str(number_of_command)
-    if commands[1][0:1] == ":" and len(commands[1]) > 2:
+    if commands[1][len(commands[1])-1:len(commands[1])] == ":":
         commands[1] = commands[1][0:len(commands[1])-1]
-        return toInt(commands[1]) + " " + str(number_of_command)
-    elif commands[2][0:1] == ":" and len(commands[2]) > 2: 
-        commands[2] = commands[2][1:len(commands[2])-1]
-        return toInt(commands[1])+ " " + str(number_of_command)
+        commands[1] =  toInt(commands[1])
+    elif commands[2][0:1] == ":":
+        commands[2] = commands[2][1:len(commands[2])]
+        commands[1] = toInt(commands[1])    
     elif commands[2] == ":":
-        del commands[2]    
-    elif set(":").intersection(commands[1]) == False or  set(":").intersection(commands[2]) == False:       
-        output = "zОшибка. После метки должно стоять \":\"f" + str(number_of_command)     
-
-
-   
-
+        commands.pop(2)        
+    elif set(":").intersection(commands[1]):
+        commands.insert(2,commands[1][commands[1].find(":")+1:len(commands[1])])
+        commands[1] = commands[1][0:commands[1].find(":")]
+    elif set(":").intersection(commands[1]) == False and set(":").intersection(commands[2]):
+        return "zОшибка. После метки должно стоять \":\"f " + str(number_of_command) 
 
     
     return " ".join(commands)    
-
-def isTochka_zapyataya(word):
-    for num,char in enumerate(word):
-            if char == ';':
-                return True
-    return False
 
 def check_for_errors(data):
     data = data.lower()
@@ -72,5 +65,5 @@ def check_for_errors(data):
 
 
 if __name__ == '__main__':
-    print(check_for_errors('Программа Ввод 4 : строка1; ввод 5 строка2; ввод 6 строка3 конец'))    
+    print(check_for_errors('Программа Ввод 4: бццц = ; ввод 5 строка2; ввод 6 строка3 конец'))    
     

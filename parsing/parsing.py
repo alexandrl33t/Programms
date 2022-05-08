24aeb34bc3ceca80dd65c395280f807b8aab4ea8
Alexandr <alexandrl33t@yandex.ru>
2022-05-08 21:18:17 +0300

    right part update

--- parsing/parsing.py
+++ parsing/parsing.py
@@ -1,18 +1,33 @@
 
+from dataclasses import replace
 from doctest import OutputChecker
 from logging import exception
 import string
 from unicodedata import digit
 import re
 
-output = "" 
+
+
 
 memory = {}
 
-class Perem():
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+
+def complited_right_part(right,number_of_command):
+    for letter in right:
+        if letter.isalpha():
+            m = re.search('[а-я][0-7][0-7][0-7]', right)
+            if not m:
+                return "zОшибка. Переменная в выражении должна быть формата бццц, где б это А!...!Я, ц это 0!...!7f" + str(number_of_command)         
+            try:
+                right = right.replace(m.group(0), str(memory[m.group(0)]))
+            except:
+                return "zОшибка. Переменной " + m.group(0) + " не существует.f" + str(number_of_command)    
+       
+    try: 
+        right = eval(right)
+    except:
+        return "Ошибка"           
+    return right
 
 def right_part(commands, number_of_command):
     #отсечение правой части 
@@ -21,13 +36,14 @@ def right_part(commands, number_of_command):
             commands = " ".join(commands)
             commands = re.split("=", commands)
         else:
-            return "zОшибка. После переменной должно стоять \"=\"f " + str(number_of_command)
+            return "zОшибка. После переменной должно стоять \"=\"f" + str(number_of_command)
     except:
         return "zОшибка. Неполная командаf" + str(number_of_command)
 
-    right = commands[1].split()
-      
-    return commands[1]
+    right = " ".join(commands[1].split())
+    answer = ''
+  
+    return complited_right_part(right, number_of_command)
 
 def toInt(command):
     try:
@@ -48,9 +64,14 @@ def command_processing(commands, number_of_command):
             commands = " ".join(commands)
             commands = commands.split()
         else:
-            return "zОшибка. После метки должно стоять \":\"f " + str(number_of_command)
+            return "zОшибка. После метки должно стоять \":\"f" + str(number_of_command)
     except:
-        return "zОшибка. Неполная команда f " + str(number_of_command)
+        return "zОшибка. После слова ввод должна идти метка f" + str(number_of_command)
+
+    try:
+        commands[1] = toInt(commands[1])
+    except:
+        return "zОшибка. Неполная команда.f"    
 
    #проверка переменной
     try:
@@ -61,7 +82,7 @@ def command_processing(commands, number_of_command):
         return "zОшибка. Переменная должна быть формата бццц, где б это А!...!Я, ц это 0!...!7f" + str(number_of_command)    
     
     memory[commands[2]] = right_part(commands, number_of_command)
-    
+    print(memory)
     return " ".join(commands)    
 
 def check_for_errors(data):
@@ -73,21 +94,24 @@ def check_for_errors(data):
         elif data_list[len(data_list)-1] != "конец":
             return  "Ошибка. Программа, должна заканчиваться словом ""Конец""" 
     except: return  "Программа пуста. "  
-    output =""
-    first_command = 1
-    number_of_command = 1
-    for i in range(1,len(data_list)-1):
-        last_command = len(data_list)-2
-        if set(';+$').intersection(data_list[i]):
-            data_list[i] = data_list[i][0:len(data_list[i])-1]
-            last_command = i
-            output += command_processing(data_list[first_command:last_command+1], number_of_command) + " "
-            first_command = i+1  
-            number_of_command +=1
-            continue
-        if i == len(data_list)-2:
-            output += command_processing(data_list[first_command:len(data_list)-1], number_of_command) + " "
-            number_of_command +=1  
+
+    if data_list[1] != 'ввод' and len(data_list) > 2:
+        return 'Звено начинается со слова \"Ввод\"'
+    
+    del data_list[0], data_list[len(data_list)-1]
+    try:
+        data_list = re.split(';', " ".join(data_list))
+    except:
+        data_list = re.split('ввод', " ".join(data_list))
+        if len(data_list) > 1:
+            return "После команды " + data_list[0] + " должна стоять \';\'"
+        else:
+            data_list[0] = 'ввод ' + data_list[0]
+    output = ''
+    for i, command in enumerate(data_list):
+        command = " ".join(command.split())
+        output += command_processing(command.split(), i) + " "
+    
     if output.find("z") > -1:
         output = output[output.find("z")+1:output.find("f")]
     return " ".join(output.split())
@@ -97,5 +121,6 @@ def check_for_errors(data):
 
 
 if __name__ == '__main__':
-    print(check_for_errors('Программа Ввод 42:А565 = 5 * 5 + ф455; ввод 55: ф455 = 6; ввод 6: к666 = 7 конец'))    
-    
+    #print(check_for_errors('Программа Ввод 42:А565 = 5 * 5 + ф455 ввод 55: ф455 = 6 ввод 6: к666 = 7 конец'))    
+    #print(check_for_errors('Программа Ввод 42:А565 = 5 * 5 + ф455 конец'))
+    print(check_for_errors('Программа Ввод 4:А565 = 5 + (5 * 5); ввод 55: ф455 = 6 * а565; ввод 6: к666 = 7 конец'))

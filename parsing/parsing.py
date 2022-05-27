0cbe31ac4230d355ef696b6734aabb829b295196
Alexandr <alexandrl33t@yandex.ru>
2022-05-27 15:03:51 +0300

    new verison

--- parsing/parsing.py
+++ parsing/parsing.py
@@ -17,55 +17,7 @@ import re
 
 memory = {}
 
-<<<<<<< HEAD
 class Check():
-=======
-def peremError(perem, number_of_command):
-    return f"zОшибка. Переменная {perem} должна быть формата бццц, где б это А!...!Я, ц это 0!...!7f" + str(number_of_command)
-
-def complited_right_part(right,number_of_command):
-    right = "".join(right.split())
-    new_right = ''
-    k = 0
-    for i in range(len(right)):
-        if k > 0:
-            k -=1
-            continue
-        if right[i].isalpha() == False:
-            new_right += right[i]
-            continue 
-        try:
-            perem = right[i:i+5]
-            regex = '[\+|\-|\*|\/|\&|\||\(|\)|\[|\]|\^]'
-            if len(perem) == 5:
-                m = re.search(f'[а-я][0-7][0-7][0-7]{regex}', perem)
-                if not m:
-                    return peremError(perem, number_of_command)   
-                perem = right[i:i+4]
-            elif len(perem) == 4:
-                m = re.search(f'[а-я][0-7][0-7][0-7]', perem)   
-                if not m:
-                    return peremError(perem, number_of_command)     
-            new_right += memory[perem]
-            k = 3                
-        except:
-            return peremError(perem, number_of_command) 
-    right = " ".join(new_right.split())
-    right = cf.check_quad(right)
-    if right.find('z') > -1:
-        return right + str(number_of_command)                   
-    isRight = cf.check(right)
-    if isRight == True:
-        try:   
-            return eval(right)
-        except ZeroDivisionError:
-                return "zОшибка. Деление на ноль. f" + str(number_of_command)      
-    else: return isRight + str(number_of_command)
-
-def right_part(commands, number_of_command):
-    #отсечение правой части 
->>>>>>> bf03fa43b14f82180dae8e83e1795d1f29dcee1f
-    
     command = ''
     data = ''
     glubina = 0
@@ -132,7 +84,6 @@ def right_part(commands, number_of_command):
         return ""         
                     
 
-<<<<<<< HEAD
     def command_processing(self, command:str):
         if not command.startswith("ввод "):
             return f"zОшибка. Звено должно начинаться со слова \'Ввод\'f{command}f1"
@@ -156,47 +107,6 @@ def right_part(commands, number_of_command):
         
         #разделение на массив команд и обработка каждой
         command_list = re.split(';', self.data)        
-=======
-    try:
-        commands[1] = str(int(commands[1]))
-    except:
-        return "zОшибка. Метка должна быть целым числом.f" + str(number_of_command)    
-   #проверка переменной
-    
-    try:
-        if " ".join(commands).find("=") > -1:
-            commands = " ".join(commands)
-            commands = re.split("=", commands)
-        else:
-            return "zОшибка. После переменной должно стоять \"=\"f" + str(number_of_command)
-    except:
-        return "zОшибка. Неполная командаf" + str(number_of_command)   
-    left = commands[0].split()
-    perem = str(left[len(left)-1])
-    try:
-        m = re.search('[а-я][0-7][0-7][0-7]', perem)
-        if not m.group():
-            return peremError(perem, number_of_command)    
-    except:
-        return peremError(perem, number_of_command)  
-    
-    memory[perem] = right_part(commands, number_of_command)
-    try: 
-        memory[perem] = str(int(memory[perem]))
-    except:
-        pass  
-    return str(perem) + " = " +str(memory[perem]) + "\n"
-
-def check_for_errors(data):
-    data = data.lower()
-    data_list = data.split()
-    try:
-        if data_list[0] != "программа":
-            return  "Ошибка. Программа должна начинаться со слова ""Программа"""
-        elif data_list[len(data_list)-1] != "конец":
-            return  "Ошибка. Программа, должна заканчиваться словом ""Конец""" 
-    except: return  "Программа пуста."  
->>>>>>> bf03fa43b14f82180dae8e83e1795d1f29dcee1f
 
         answer = 'Результат выполнения команд: \n'
         for i in range(len(command_list)):
@@ -221,15 +131,9 @@ def check_for_errors(data):
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     #print(check_for_errors('Программа 42:а455 = [[455 + 3]] + 350 конец'))    
     #a = Check('Программа Ввод 42:А565 = (61 конец')
     a = Check('Программа Ввод 55 : А565 = 5 / 5; ввод 55 : ф455 =а565 + 5 ; ввод 6: к666 = 7 + [а565 + ф455] + 100 конец')
     #a = Check('Программа ввод 42:а455 = 455 + 3 + ~350; ввод 55:ы222 = [а455+4] + 5 ; конец')
     #a = Check('Программа ввод 42: а666 = s   конец')
     print(a.check_for_errors())
-=======
-    #print(check_for_errors('Программа ввод 42:а455 = [[455 + 3]] ^ 350 конец'))    
-   # print(check_for_errors('Программа Ввод 42:А565 = 5 * 5 + 20 конец'))
-    print(check_for_errors('Программа Ввод 55:А565 = 5 / 5; ввод 55: ф455 = 6 + а565; ввод 6: к666 = 7 + а565 + ф455 + 100; конец'))
->>>>>>> bf03fa43b14f82180dae8e83e1795d1f29dcee1f

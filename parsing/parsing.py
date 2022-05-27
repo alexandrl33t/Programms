384a8bd539aefdcb5124dd7f5393989028bf3c35
Alexandr <alexandrl33t@yandex.ru>
2022-05-27 14:58:20 +0300

    new version

--- parsing/parsing.py
+++ parsing/parsing.py
@@ -1,155 +1,140 @@
 
 
+from copy import copy
 from dataclasses import replace
+from distutils.filelist import findall
 from doctest import OutputChecker
+from genericpath import exists
 from logging import exception
 from ntpath import join
 import string
+from turtle import right
 from unicodedata import digit
 import re
-import check_fo_errors_eval as cf
+import right_part as rp
 
-
+import re
 
 memory = {}
 
-
-def complited_right_part(right,number_of_command):
-    for letter in right:
-        if letter.isalpha():
-            m = re.search('[а-я][0-7][0-7][0-7]', right)
-            if not m:
-                return "zОшибка. Переменная в выражении должна быть формата бццц, где б это А!...!Я, ц это 0!...!7f" + str(number_of_command)         
-            try:
-                right = right.replace(m.group(0), str(memory[m.group(0)]))
-            except:
-                return "zОшибка. Переменная " + m.group(0) + " не объявлена.f" + str(number_of_command)      
-    right = " ".join(right.split())
-    right = cf.check_quad(right)
-    if right.find('z') > -1:
-        return right + str(number_of_command)                   
-    isRight = cf.check(right)
-    if isRight == True:
-        try:   
-            return eval(right)
-        except ZeroDivisionError:
-                return "zОшибка. Деление на ноль. f" + str(number_of_command)      
-    else: return isRight + str(number_of_command)
-
-def right_part(commands, number_of_command):
-    #отсечение правой части 
+class Check():
     
+    command = ''
+    data = ''
+    glubina = 0
+    skobka_is_opened = False
+    def __init__(self, data):
+        self.command = ''
+        self.data = data
+        self.glubina = 0
+        self.skobka_is_opened = False
 
-    right = " ".join(commands[1].split())
-  
-    return complited_right_part(right, number_of_command)
+    def calculate(self, command:str, last_word):
+        if len(command) == 0:
+            if last_word == 'metka' or last_word == 'metka space':
+                return f"zОшибка. После метки должно стоять \':\''f{self.command}f1"
+            if last_word == ':' or last_word == ': space' :
+                return f"zОшибка. После \':\' должна идти переменная'f{self.command}f1"  
 
+        for i in range(len(command)):
+            if last_word == 'metka space':
+                if command[i] == ":":
+                    return self.calculate(command[i+1:len(command)], ':')
+                else:
+                    return f"zОшибка. После метки должно стоять \':\''f{self.command}f1" 
 
-def command_processing(commands, number_of_command):
-    if commands[0] != "ввод":
-        return  "zОшибка. Звено должно содержать слово ввод. f" + str(number_of_command)
-    
-    #проверка метки
-    try:
-        if commands[1].find(":") > -1:
-            commands = " ".join(commands)
-            commands = re.split(":", commands)
-            for command in commands:
-                if len(command) == 0 and i != len(command)-1:
-                    return "zОшибка. Дублируется \':\' f" + str(number_of_command)
-            commands = " ".join(commands)
-            commands = commands.split()
-        else:
-            try:
-                if commands[2].find(":") > -1:
-                    commands = " ".join(commands)
-                    commands = re.split(":", commands)
-                    for i,command in enumerate(commands):
-                        if len(command) == 0 and i != len(command)-1:
-                            return "zОшибка. Дублируется \':\' f" + str(number_of_command) 
-                    commands = " ".join(commands)
-                    commands = commands.split() 
-            except:
-                return "zОшибка. После метки должно стоять \":\"f" + str(number_of_command)
-    except:
-        return "zОшибка. После слова ввод должна идти метка f" + str(number_of_command)
+            if last_word == 'metka':
+                if command[i].isdigit():
+                        return self.calculate(command[i+1:len(command)], 'metka') 
+                elif command[i] != ":" and not command[i].isspace():
+                    return f"zОшибка. Метка должна быть целочисленной'f{self.command}f1"                 
+                elif command[i].isspace():
+                    return  self.calculate(command[i+1:len(command)], 'metka space')              
+                elif command[i] == ":":
+                    return  self.calculate(command[i+1:len(command)], ':') 
+            elif last_word == ':' or last_word == ': space':
+                if command[i].isalpha():
+                    perem = command[i:i+4]
+                    m = re.search('[а-я][0-7][0-7][0-7]', perem)   
+                    if not m:
+                        return f"zОшибка. Переменная должна быть формата \'БЦЦЦ\', где Б = А!Б!...!Я, Ц = 0!1!...!7'f{self.command}f1"
+                    try:
+                        if command[i+4] == " ":
+                            if command [i+5] == '=':
+                                memory[perem] =  rp.right_calculate(command[i+6:len(command)], memory)
+                                if memory[perem].find("z")>-1:
+                                    return memory[perem][memory[perem].find("z"):memory[perem].find("f")+1] + self.command + "f1"   
+                                return ""
+                            else: 
+                                return f"zОшибка. После переменной должно стоять \'=\'f{self.command}f1" 
+                        elif command[i+4] == '=':
+                            memory[perem] = rp.right_calculate(command[i+5:len(command)], memory)
+                            if memory[perem].find("z")>-1:
+                                return memory[perem][memory[perem].find("z"):memory[perem].find("f")+1] + self.command + "f1" 
+                            return ""
+                        else: 
+                            return f"zОшибка. Переменная должна быть формата \'БЦЦЦ\', где Б = А!Б!...!Я, Ц = 0!1!...!7'f{self.command}f1"    
+                    except:
+                        return f"zОшибка. После переменной должно стоять \'=\'f{self.command}f1" 
+                elif command[i].isspace():
+                     return  self.calculate(command[i+1:len(command)], ': space')
+                elif command[i] == ':':
+                        return 'zОшибка. Дублируется \':\'f'      
+                else: 
+                    return f"zОшибка. Переменная должна быть формата \'БЦЦЦ\', где Б = А!Б!...!Я, Ц = 0!1!...!7'f{self.command}f1"  
+        return ""         
+                    
 
-    try:
-        commands[1] = str(int(commands[1]))
-    except:
-        return "zОшибка. Метка должна быть целым числом.f" + str(number_of_command)    
-   #проверка переменной
-    
-    try:
-        if " ".join(commands).find("=") > -1:
-            commands = " ".join(commands)
-            commands = re.split("=", commands)
-        else:
-            return "zОшибка. После переменной должно стоять \"=\"f" + str(number_of_command)
-    except:
-        return "zОшибка. Неполная командаf" + str(number_of_command)   
-    left = commands[0].split()
-    perem = str(left[len(left)-1])
-    try:
-        m = re.search('[а-я][0-7][0-7][0-7]', perem)
-        if not m.group():
-            return "zОшибка. Переменная должна быть формата бццц, где б это А!...!Я, ц это 0!...!7f" + str(number_of_command)    
-    except:
-        return "zОшибка. Переменная должна быть формата бццц, где б это А!...!Я, ц это 0!...!7f" + str(number_of_command)    
-    
-    memory[perem] = right_part(commands, number_of_command)
-    try: 
-        memory[perem] = str(int(memory[perem]))
-    except:
-        pass  
-    return str(perem) + " = " +str(memory[perem]) + "\n"
+    def command_processing(self, command:str):
+        if not command.startswith("ввод "):
+            return f"zОшибка. Звено должно начинаться со слова \'Ввод\'f{command}f1"
+        command = command[command.find("ввод ")+5:len(command)]
+        
+        return self.calculate(command, 'metka') 
 
-def check_for_errors(data):
-    data = data.lower()
-    data_list = data.split()
-    try:
-        if data_list[0] != "программа":
-            return  "Ошибка. Программа должна начинаться со слова ""Программа"""
-        elif data_list[len(data_list)-1] != "конец":
-            return  "Ошибка. Программа, должна заканчиваться словом ""Конец""" 
-    except: return  "Программа пуста. "  
+    def check_for_errors(self):
+        self.data = self.data.lower()
+        self.data = " ".join(self.data.split())
+        #начинается со слов программа и конец
+        if not self.data.startswith("программа"):
+            return "zОшибка. Программа должна начинаться со слова \'Программа\'f"
+        if not re.search('.*[\s|\\n]*(конец)$', self.data):
+            return "zОшибка. Программа должна заканчиваться словом \'Конец\'f"
+        #проверка наличия разделителя
+        self.data = " ".join(self.data.split()[1:-1])
+        if len(re.findall('ввод', self.data)) - len(re.findall(';', self.data)) > 1:
+            place = re.findall('\S*[\w|\)|\]]\s*ввод', self.data)[0]
+            return "zОшибка. Отсутствует \';\' между \"" + place + "\""
+        
+        #разделение на массив команд и обработка каждой
+        command_list = re.split(';', self.data)        
 
-    if len(data_list) == 2:
-        return  "Программа пуста. " 
+        answer = 'Результат выполнения команд: \n'
+        for i in range(len(command_list)):
+            command_list[i] = " ".join(command_list[i].split())
+            self.command = command_list[i]
+            if len(command_list[i]) == 0:
+                try:
+                    len(command_list[i+1])
+                    return "zОшибка. Дублируется \';\'f;;f1"
+                except:    
+                    continue
+            answer += self.command_processing(command_list[i])
 
-    if data_list[1] != 'ввод' and len(data_list) > 2:
-        return 'Звено начинается со слова \"Ввод\"'      
-    
-
-    del data_list[0], data_list[len(data_list)-1]
-
-   
-    data_list = re.split('ввод', " ".join(data_list))
-    if len(data_list) > 1:
-        for i in range(1,len(data_list)):
-            data_list[i] = "ввод" + data_list[i]
-            if data_list[i].find(";") > -1 or i == len(data_list)-1:
-                continue
-            return "zПосле команды \'" + data_list[i] + "\' должна стоять \';\'f" + str(i-1)
-
-        del data_list[0]
-    else:
-        data_list[0] = 'ввод ' + data_list[0]      
-    data_list = " ".join(data_list)
-    data_list = re.split(";", data_list) 
-    for i in range(len(data_list)): 
-        data_list[i] = " ".join(data_list[i].split())   
-    output = 'Результаты выполнения программы: '
-    for i, command in enumerate(data_list):
-        command = " ".join(command.split())
-        output += command_processing(command.split(), i) + " "
-    return " ".join(output.split())
+        for item in memory.keys():
+            answer += item + " = " + str(memory[item]) + " " 
+        if answer.find("z") > -1:
+            return answer[answer.find("z"):answer.find('f1')+2]                          
+        return  answer
 
 
     
 
 
 if __name__ == '__main__':
-    print(check_for_errors('Программа 42:а455 = [[455 + 3]] + 350 конец'))    
-   # print(check_for_errors('Программа Ввод 42:А565 = 5 * 5 + 20 конец'))
-    #print(check_for_errors('Программа Ввод 55:А565 = 5 / 5; ввод 55: ф455 = 6 + а565; ввод 6: к666 = 7 + а565 + ф455 + 100 конец'))
+    #print(check_for_errors('Программа 42:а455 = [[455 + 3]] + 350 конец'))    
+    #a = Check('Программа Ввод 42:А565 = (61 конец')
+    a = Check('Программа Ввод 55 : А565 = 5 / 5; ввод 55 : ф455 =а565 + 5 ; ввод 6: к666 = 7 + [а565 + ф455] + 100 конец')
+    #a = Check('Программа ввод 42:а455 = 455 + 3 + ~350; ввод 55:ы222 = [а455+4] + 5 ; конец')
+    #a = Check('Программа ввод 42: а666 = s   конец')
+    print(a.check_for_errors())
---
+++ parsing/right_part.py
@@ -0,0 +1,120 @@
+import parsing as pr
+import re
+
+def right_calculate(data:str, memory:dict):
+    data = " ".join(data.split())
+    k = 0
+    if len(data) == 0:
+        return "zОшибка. В правой части ничего нет.f"
+    #замена переменных если есть
+    new_data = ''    
+    regex_after_perem = '\s|\+|\-|\*|\/|\&|\||\)|\]'
+    regex_operators = '\+|\-|\*|\/|\&|\|'                          
+    regex_start_with = '^[\-|\(|\[|а-я|\d|\~]'
+    if not re.search(regex_start_with, data):
+        if data[0] == '=':
+            return f"zОшибка. Дублируется \'{data[0]}\'f"
+        return f"zОшибка. Выражение не может начинаться с \'{data[0]}\'f"
+    for i in range(len(data)):
+        if k > 0:
+            k-=1
+            continue 
+        if data[i].isspace():
+            continue
+        #замена переменной
+        if data[i].isalpha():
+                perem = data[i:i+5]
+                if len(perem) < 5:  
+                    m = re.search('[а-я][0-7][0-7][0-7]', perem)  
+                    if not m:
+                        return f"zОшибка. Переменная должна быть формата \'БЦЦЦ\', где Б = А!Б!...!Я, Ц = 0!1!...!7'f"
+                    try:
+                        new_data += str(memory[perem])
+                        break
+                    except:
+                        return f"zОшибка. Переменная {perem} не объявлена.f"   
+                elif len(perem) == 5:
+                    m = re.search(f'[а-я][0-7][0-7][0-7][{regex_after_perem}]', perem)  
+                    if not m:
+                        return f"zОшибка. Переменная должна быть формата \'БЦЦЦ\', где Б = А!Б!...!Я, Ц = 0!1!...!7'f"
+                    try:
+                        if data[i+4].isdigit() or data[i+4].isspace() and data[i+5].isdigit():
+                            return "zОшибка. После переменной не может идти числоf"    
+                        perem = perem[0:4]
+                        new_data += str(memory[perem])
+                        k = 3
+                        continue
+                    except:
+                        return f"zОшибка. Переменная {perem} не объявлена.f"  
+        #работа с круглыми скобками                         
+        if data[i] == "(":
+            if data.rfind(")") > -1 and data.rfind(')') > i:
+                string_inside = right_calculate(data[i+1:data.rfind(')')], memory)
+                if string_inside.find('z') > -1:
+                    return string_inside
+                new_data += f"({string_inside})"    
+                k =  data.rfind(')') - i
+                continue      
+            else:
+                return "zОшибка. Отсутствует закрывающая скобка \')\'f"
+        if data[i] == ')':
+             return "zОшибка. Отсутствует открывающая скобка \'(\'f" 
+        if data[i] == '[':
+            if data.rfind("]") > -1 and data.rfind(']') > i:
+                try:
+                    memory['glubina'] +=1
+                    if memory['glubina'] > 2:
+                        return "zОшибка. Глубина вложенности у квадратных скобок - 2f"
+                except:
+                    memory['glubina'] = 1             
+                string_inside = right_calculate(data[i+1:data.rfind(']')], memory)
+                if string_inside.find('z') > -1:
+                    return string_inside
+                new_data += f"({string_inside})"    
+                k =  data.rfind(']') - i
+                continue 
+            else:
+                return "zОшибка. Отсутствует закрывающая скобка \']\'f"
+        if data[i] == ']':
+             return "zОшибка. Отсутствует открывающая скобка \'[\'f" 
+        if data[i] == '~':
+            if i != len(data)-1:
+                if data[i+1] == " ":
+                    m = re.search(regex_operators, data[i+2])
+                    if m:
+                        return f"zОшибка. \'~\' не может стоять перед оператором {data[i+2]}.f"
+                else:
+                    m = re.search(regex_operators, data[i+1])
+                    if m:
+                        return f"zОшибка. \'~\' не может стоять перед оператором \'{data[i+1]}\'.f"        
+            else: 
+                return "zОшибка. Выражение не может заканчиваться на \'~\'f"
+        if not data[i].isdigit() and not data[i].isalpha():
+            if re.search(regex_operators, data[i]):
+                if i != len(data)-1:
+                    if data[i+1] == " ":
+                        if re.search(regex_operators, data[i+2]):
+                            return "zОшибка. Два оператора не могут идти друг за другом.f"
+                    else:
+                        if re.search(regex_operators, data[i+1]):
+                            return "zОшибка. Два оператора не могут идти друг за другом.f"       
+                else:
+                    return f"zОшибка. Оператор {data[i]} не может стоять в конце выражения.f"    
+            elif data[i] != '~': 
+                return f"zОшибка. Недопустимый символ \'{data[i]}\'f"    
+        if data[i].isdigit():
+            if int(data[i])> 7:
+                return f"zОшибка. Цифра {data[i]} не в восьмеричной системе.f" 
+            if i != len(data)-1:
+                if data[i+1] == " ":
+                    if data[i+2].isdigit():
+                        return f"zОшибка. Два чила не могут идти друг за другом.f"       
+
+        new_data += data[i] 
+    try:
+        del memory['glubina']
+    except:
+        pass
+    if new_data.find("z") == -1:
+        return str(int(eval(new_data)))    
+    return new_data
